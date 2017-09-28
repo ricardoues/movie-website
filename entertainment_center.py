c362b37c4bd16d26ba1e88b90c399b499faf40a1
Ricardo Rios <gambitorey@gmail.com>
2017-09-28 11:50:17 -0600

    28/08/2017 11:50

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,42 +1,46 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", 
-                        "A story of a boy and his toy come to life", 
-                        "https://a.dilcdn.com/bl/wp-content/uploads/sites/8/2013/02/toy_story_wallpaper_by_artifypics-d5gss19.jpg", 
-                        "https://www.youtube.com/watch?v=nCqtQLmJTl0"
-                       )
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toy come to life",
+                        "https://goo.gl/LndV7F",
+                        "https://www.youtube.com/watch?v=nCqtQLmJTl0")
 
+thor_ragnarok = media.Movie("Thor Ragnarok",
+                            "I don't know yet",
+                            "https://goo.gl/r7MpL7",
+                            "https://www.youtube.com/watch?v=v7MGUNV8MxU")
 
-thor_ragnarok = media.Movie("Thor Ragnarok", 
-                            "I don't know yet", 
-                            "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg", 
-                            "https://www.youtube.com/watch?v=v7MGUNV8MxU") 
-
-imitation_game = media.Movie("Imitation Game", 
-                             "A computer scientist needs to break the codes of the enigma machine", 
-                             "https://upload.wikimedia.org/wikipedia/en/5/5e/The_Imitation_Game_poster.jpg", 
+imitation_game = media.Movie("Imitation Game",
+                             "A computer scientist needs \
+                              to break the codes of the \
+                              enigma machine",
+                             "https://goo.gl/1x78fT",
                              "https://www.youtube.com/watch?v=S5CjKEFb-sM")
 
 dunkirk = media.Movie("Dunkirk",
-                      "This movie recreates the events behind the battle of dunkirk in the World War II",
-                      "https://upload.wikimedia.org/wikipedia/en/1/15/Dunkirk_Film_poster.jpg",
-                      "https://www.youtube.com/watch?v=F-eMt3SrfFU")
+                      "This movie recreates the events \
+                       behind the battle of dunkirk \
+                       in the World War II",
+                      "https://goo.gl/3VS61H",
+                      "https://www.youtube.com/\
+                       watch?v=F-eMt3SrfFU")
 
-war_for_planet_apes = media.Movie("War for the Planet of the Apes", 
-                                  "Cesar and his apes fight against an Human Army",
-                                  "https://upload.wikimedia.org/wikipedia/en/d/d7/War_for_the_Planet_of_the_Apes_poster.jpg", 
-                                  "https://www.youtube.com/watch?v=JDcAlo8i2y8")
-
+war_for_planet_apes = media.Movie("War for the Planet of the Apes",
+                                  "Cesar and his apes fight \
+                                   against an Human Army",
+                                  "https://goo.gl/51cijY",
+                                  "https://www.youtube.com/\
+                                   watch?v=JDcAlo8i2y8")
 
 rogue_one = media.Movie("Rogue One",
-                        "The Death Star plans were captured by the Rebel Alliance", 
-                        "https://upload.wikimedia.org/wikipedia/en/d/d4/Rogue_One%2C_A_Star_Wars_Story_poster.png",
-                        "https://www.youtube.com/watch?v=frdj1zb9sMY&t=57s")
+                        "The Death Star plans were \
+                         captured by the Rebel Alliance",
+                        "https://goo.gl/7EJJEx",
+                        "https://www.youtube.com/watch?v=frdj1zb9sMY")
 
-
-movies = [toy_story, thor_ragnarok, imitation_game, dunkirk, war_for_planet_apes, rogue_one]
+movies = [toy_story, thor_ragnarok, imitation_game,
+          dunkirk, war_for_planet_apes, rogue_one]
 
 fresh_tomatoes.open_movies_page(movies)
-
 
--- media.py
+++ media.py
@@ -1,9 +1,21 @@
 import webbrowser
 
-class Movie():
+class Movie(object):
       """This class is for storing movie information """
+
       def __init__(self, movie_title, movie_storyline, 
                    poster_image, trailer_youtube):
+          """ 
+          Initialize the movie instance. 
+          Arguments:
+          self: refers to the current instance
+          movie_title: title of the movie 
+          movie_storyline: the plot of the movie 
+          poster_image: poster of the movie 
+          trailer_youtube: url of the trailer movie 
+          Returns:
+          None 
+          """ 
 
           self.title = movie_title
           self.storyline = movie_storyline
@@ -11,6 +23,13 @@ class Movie():
           self.trailer_youtube_url = trailer_youtube
 
       def show_trailer(self):
+          """
+          Show the movie trailer. 
+          Arguments: 
+          self: refers to the current instance
+          Returns:
+          None
+          """ 
           webbrowser.open(self.trailer_youtube_url)
 
 
